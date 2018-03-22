a738555ec753412d0cc38b868ad46b3f0b5038a2
mapmelad <AbSemyon@gmail.com>
2018-03-22 12:25:11 +0300

    Initial Commit

---
+++ vkPhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vkPhoto
+//
+//  Created by Semyon on 22.03.2018.
+//  Copyright © 2018 Semyon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
